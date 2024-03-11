c91a589d67f2e40984b971f4225edc4a1be11de9
Gabriel Cardoso Tavares <gabrielcardosotavares@gmail.com>
2024-03-11 10:27:20 -0300

    update cliente routes with validation cpf and get user and update data user

--- routes/clientRoutes.ts
+++ routes/clientRoutes.ts
@@ -1,22 +1,73 @@
 import express from 'express'
 
+import { CPFValidator } from './../utils/CPFValidator'
 import ClientModel from './models/clientModel'
 
 const router = express.Router()
 
-//Consulta base de usuarios
-router.get('/', (req, res) => {
-  res.send('Rota de usuários')
+interface FormatClient {
+  cpf: string
+  firstName: string
+  lastName: string
+  email: string
+  phone: string
+}
+
+//Consulta base de clientes
+router.get('/', async (req, res) => {
+  const clients = await ClientModel.find()
+
+  if (clients.length == 0) {
+    res.status(400)
+    res.send({ message: 'Nem um usuario cadastrado' })
+  }
+
+  const array: Array<FormatClient> = new Array<FormatClient>()
+  clients.forEach(element => {
+    const item: FormatClient = {
+      cpf: element.cpf,
+      firstName: element.firstName,
+      lastName: element.lastName,
+      email: element.email,
+      phone: element.phone
+    }
+    array.push(item)
+  })
+
+  res.status(200)
+  res.send({ clients: array })
 })
 
-//Obtem usuario
-router.get('/:id', (req, res) => {
-  const userId = req.params.id
-  res.send(`Detalhes do usuário com ID ${userId}`)
+//Obtem cliente
+router.get('/:id', async (req, res) => {
+  const client = await ClientModel.find({ cpf: req.params.id })
+
+  if (client.length == 0) {
+    res.status(400)
+    res.send({ message: 'Nem um cliente cadastrado com cpf informado' })
+  } else if (client.length == 2) {
+    res.status(400)
+    res.send({ message: 'Mais de um cliente cadastrado com cpf informado' })
+  }
+
+  const array: Array<FormatClient> = new Array<FormatClient>()
+  client.forEach(element => {
+    const item: FormatClient = {
+      cpf: element.cpf,
+      firstName: element.firstName,
+      lastName: element.lastName,
+      email: element.email,
+      phone: element.phone
+    }
+    array.push(item)
+  })
+
+  res.status(200)
+  res.send({ client: array })
 })
 
-//Cadatra usuario
-router.post('/', (req, res) => {
+//Cadatra cliente
+router.post('/', async (req, res) => {
   const clientModel = new ClientModel({
     cpf: req.body.cpf,
     firstName: req.body.firstName,
@@ -26,32 +77,67 @@ router.post('/', (req, res) => {
     phone: req.body.phone
   })
 
+  const isValidCPF = new CPFValidator().handle(req.body.cpf)
+  if (!isValidCPF) {
+    res.status(401)
+    res.send({ message: 'CPF invalido' })
+  }
+
+  let cli = await ClientModel.findOne({ cpf: req.body.cpf })
+  if (cli) {
+    res.status(401)
+    res.send({ message: 'CPF ja cadastrado' })
+  }
+
+  cli = await ClientModel.findOne({ email: req.body.email })
+  if (cli) {
+    res.status(401)
+    res.send({ message: 'Email ja cadastrado' })
+  }
+
   clientModel
     .save()
     .then(() => {
-      res.send('Cliente cadastrado com sucesso!')
+      res.status(201)
+      res.send({ message: 'Cliente cadastrado com sucesso!' })
     })
     .catch(error => {
       res.status(400)
-      res.send('Erro ao cadastrar cliente:' + error)
+      res.send({ message: 'Erro ao cadastrar cliente:' + error })
     })
 })
 
-//Atualiza email e senha
-router.patch('/', (req, res) => {
+//Atualiza email
+router.patch('/updateEmail', (req, res) => {
   try {
     ClientModel.findOneAndUpdate(
       { cpf: req.body.cpf },
       {
         $push: {
-          email: req.body.email,
+          email: req.body.email
+        }
+      }
+    )
+    res.send({ message: `Email do cliente atualizado com sucesso.` })
+  } catch {
+    res.send({ message: `Nao foi possivel atualizar o email do usuario.` })
+  }
+})
+
+//Atualiza senha
+router.patch('/updatePassword', (req, res) => {
+  try {
+    ClientModel.findOneAndUpdate(
+      { cpf: req.body.cpf },
+      {
+        $push: {
           password: req.body.password
         }
       }
     )
-    res.send(`Email e senha atualizados com sucesso.`)
+    res.send({ message: `Senha do cliente atualizado com sucesso.` })
   } catch {
-    res.send(`Nao foi possivel atualizar o cadastro do usuario.`)
+    res.send({ message: `Nao foi possivel atualizar o senha do usuario.` })
   }
 })
 
