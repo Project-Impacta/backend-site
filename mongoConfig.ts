30f648d60f27eba570df414e7aec3bf38ed3d9b2
Gabriel Cardoso Tavares <gabrielcardosotavares@gmail.com>
2024-03-17 18:01:52 -0300

    conection wifh mongo db atlas wifh env vars

--- mongoConfig.ts
+++ mongoConfig.ts
@@ -1,14 +1,45 @@
 import mongoose from 'mongoose'
 
+import { MONGO_PASS, MONGO_STRING, MONGO_USER, NODE_ENV } from './enviroment'
+
 const MONGO_URI =
-  'mongodb+srv://gabrielcardosotavares:colocar_senha_aqui@impactastore.pb1hj7e.mongodb.net/local_db?retryWrites=true&w=majority&appName=ImpactaStore'
+  'mongodb+srv://gabrielcardosotavares:colocar_senha_aqui@impactastore.pb1hj7e.mongodb.net/local_impacta?retryWrites=true&w=majority&appName=ImpactaStore'
 
 const connectToMongoDB = async () => {
-  try {
-    await mongoose.connect(MONGO_URI)
-    console.log('Conectado ao MongoDB')
-  } catch (error) {
-    console.error('Erro ao conectar ao MongoDB:', error)
+  switch (NODE_ENV) {
+    case 'local':
+      try {
+        await mongoose.connect(MONGO_STRING, {
+          user: MONGO_USER,
+          pass: MONGO_PASS,
+          connectTimeoutMS: 10000
+        })
+        console.log('Conectado ao MongoDB')
+      } catch (error) {
+        console.error('Erro ao conectar ao MongoDB:', error)
+      }
+      break
+    case 'dev':
+      // TODO QUANDO IMPLEMENTAR NA ESTEIRA TEM QUE VER COMO ACESSAR AS VARIAIVEIS DE AMBIENTE APARTIR DOQ FOI CADASTRADO NA ESTEIRA
+      try {
+        await mongoose.connect(MONGO_STRING, {
+          user: MONGO_USER,
+          pass: MONGO_PASS,
+          connectTimeoutMS: 10000
+        })
+        console.log('Conectado ao MongoDB Cloud dev')
+      } catch (error) {
+        console.error('Erro ao conectar ao MongoDB Cloud:', error)
+      }
+      break
+    default:
+      // TODO QUANDO IMPLEMENTAR NA ESTEIRA TEM QUE VER COMO ACESSAR AS VARIAIVEIS DE AMBIENTE APARTIR DOQ FOI CADASTRADO NA ESTEIRA
+      try {
+        await mongoose.connect(MONGO_URI)
+        console.log('Conectado ao MongoDB Cloud default - sem senha')
+      } catch (error) {
+        console.error('Erro ao conectar ao MongoDB:', error)
+      }
   }
 }
 
