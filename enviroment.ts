2ed40ec3968c8901d2176632b4357850ff88c4f0
Gabriel Cardoso Tavares <gabrielcardosotavares@gmail.com>
2024-04-16 22:11:06 -0300

    add variables in enviroment jwt origin token end cors

--- enviroment.ts
+++ enviroment.ts
@@ -12,3 +12,7 @@ export const NODE_ENV = process.env.NODE_ENV || ENV_VARS.LOCAL
 export const MONGO_USER = process.env.MONGO_USER || 'root'
 export const MONGO_PASS = process.env.MONGO_PASS || '123456'
 export const MONGO_STRING = process.env.MONGO_STRING || 'mongodb://mongo_impacta:27017/local_impacta?authSource=admin'
+export const JWT_PASS = process.env.JWT_PASS || '123456'
+export const FRONTEND_ORIGIN = process.env.FRONTEND_ORIGIN || 'frontend'
+export const FRONTEND_TOKEN = process.env.FRONTEND_TOKEN || '123456'
+export const CORS_URL = process.env.CORS_URL || 'http://localhost:3000;https://localhost:3000'
