ef175abcfdc888df4eb1b30d4feef1d051a98651
Gabriel Cardoso Tavares <gabrielcardosotavares@gmail.com>
2024-04-21 00:06:14 -0300

    change header field origin for secret origin

--- middlewares/authFrontend.ts
+++ middlewares/authFrontend.ts
@@ -4,8 +4,8 @@ import { FRONTEND_ORIGIN, FRONTEND_TOKEN } from '../enviroment'
 import { UnauthorizedError } from '../utils/APIError'
 
 export const authFrontend = async (req: Request, res: Response, next: NextFunction) => {
-  const { token, origin } = req.headers
-  if (origin != FRONTEND_ORIGIN || token != FRONTEND_TOKEN) {
+  const { secret_origin, token } = req.headers
+  if (secret_origin != FRONTEND_ORIGIN || token != FRONTEND_TOKEN) {
     throw new UnauthorizedError('Nao autorizado')
   }
   next()
