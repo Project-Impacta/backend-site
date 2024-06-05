476915d929afffc835240de05d8d96d55cd97c7d
Gabriel Cardoso Tavares <gabrielcardosotavares@gmail.com>
2024-06-05 11:32:19 -0300

    update logic product image for hash implementation

--- routes/models/productImageModel.ts
+++ routes/models/productImageModel.ts
@@ -2,12 +2,14 @@ import { model, Schema } from 'mongoose'
 
 interface ProductImage extends Document {
   productId: string
-  path: string
+  hash: string
+  mimetype: string
 }
 
 const productImageSchema = new Schema<ProductImage>({
   productId: { type: String, required: true },
-  path: { type: String, required: true }
+  hash: { type: String, required: true },
+  mimetype: { type: String, required: true }
 })
 
 export const ProductImageModel = model<ProductImage>('ProductImage', productImageSchema)
--- routes/productsImageRoutes.ts
+++ routes/productsImageRoutes.ts
@@ -6,29 +6,41 @@ import path from 'path'
 import { BadResquestError, NotFoundError } from '../utils/APIError'
 import ProductImageModel from './models/productImageModel'
 import ProductModel from './models/productModel'
+import { createHash } from 'crypto';
 
 const router = express.Router()
 
+function calculateHash(filepath: string): string {
+  const hasher = createHash('sha256');
+  const fileBuffer = fs.readFileSync(filepath);
+  hasher.update(fileBuffer);
+  return hasher.digest('hex');
+}
+
+// Cadastra hash da imagem
 router.post('/', async (req, res) => {
   const file = req.file
   if (!file) {
-    throw new BadResquestError('No file uploaded.')
+    throw new BadResquestError('Nem um arquivo enviado para o cadastro.')
   }
+  const hashFile = calculateHash(file!.path)
+  const mimetype = file.mimetype
+  fs.unlinkSync(file.path)
   if (!isObjectIdOrHexString(req.body.productId)) {
-    throw new BadResquestError('Format of productId invalid')
+    throw new BadResquestError('Formato do productId invalido')
   }
   const product = await ProductModel.find({ _id: req.body.productId })
   if (product.length == 0) {
-    fs.unlinkSync(file.path)
     throw new NotFoundError('Nem um produto cadastrado com id informado')
   }
   const productImage = await ProductImageModel.find({ productId: req.body.productId })
   if (productImage.length > 0) {
-    throw new BadResquestError('ProductId informed already have image, exclude old and try again.')
+    throw new BadResquestError('ProductId ja tem uma imagem, exclua a antiga e tente novamente.')
   }
   const newImage = new ProductImageModel({
     productId: req.body.productId,
-    path: file!.path
+    hash: hashFile,
+    mimetype: mimetype
   })
   newImage
     .save()
@@ -36,47 +48,49 @@ router.post('/', async (req, res) => {
       return res.status(201).json(newImage)
     })
     .catch(error => {
-      fs.unlinkSync(file.path)
-      throw new BadResquestError('Erro ao cadastrar cliente:' + error)
+      throw new BadResquestError('Erro ao cadastrar imagem:' + error)
     })
 })
 
+// Consulta imagem pelo id do produto
 router.get('/:id', async (req, res) => {
   const { id } = req.params
 
   const image = await ProductImageModel.find({ productId: id })
-  if (!image) {
-    throw new NotFoundError('Image not found.')
+  console.log('image->',image)
+  if (image.length == 0) {
+    throw new NotFoundError('Imagem nao encontrada com o id informado.')
   }
 
-  return res.sendFile(path.resolve(image[0].path))
+  return res.json({hash: image[0].hash})
 })
 
+// Consulta todas imagens
 router.get('/', async (req, res) => {
   const find = await ProductImageModel.find()
   if (find.length == 0) {
-    throw new NotFoundError('No has images on collection.')
+    throw new NotFoundError('Nao tem imagens cadastradas.')
   }
 
   return res.json({ data: [find] })
 })
 
+// Deleta hash da imagem do banco
 router.delete('/', async (req, res) => {
   const { productId } = req.body
   const find = await ProductImageModel.find()
   if (find.length == 0) {
-    throw new NotFoundError('No has images on collection.')
+    throw new NotFoundError('Nao tem imagens cadastradas.')
   }
   if (productId) {
     const image = await ProductImageModel.find({ productId: productId })
     if (!image) {
-      throw new NotFoundError('No has image by productId on collection.')
+      throw new NotFoundError('Imagem nao encontrada com o id informado.')
     }
-    fs.unlinkSync(image[0].path)
     await image[0].deleteOne()
-    return res.json({ message: 'File is removed' })
+    return res.json({ message: 'Arquivo removido' })
   }
-  throw new BadResquestError('Bad request return, analyse and try again.')
+  throw new BadResquestError('Erro de requisicao, analise e tente novamente.')
 })
 
 export default router
